abcbedbfbd540e6212d14c36813f21a5900d9351
RizkyAlfiansyah <rizkkyaf@gmail.com>
2021-12-20 02:35:28 +0800

    eighth commit

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ window.addEventListener("scroll", () => {
 
 var myIndex = 0;
 carousel();
-carousel2();
 
 function carousel() {
   var i;
@@ -56,15 +55,4 @@ function carousel() {
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 2000); // Change image every 2 seconds
-}
-function carousel2() {
-  var i;
-  var x = document.getElementsByClassName("mySlides2");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel2, 2000); // Change image every 2 seconds
 }
